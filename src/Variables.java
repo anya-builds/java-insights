c8ec5a28578ecf4c574b40f7f8e0f5fc579b5df6
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-11-11 23:17:35 +0530

    variables

---
+++ src/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main() {
+        // variables
+        int a;
+        String name;
+
+        a=10;
+        name="Ankita";
+        System.out.println(a);
+        System.out.println(name);
+
+        int age=20;
+        System.out.println("hello " + name + ". your are " + age + "years old");
+    }
+}
