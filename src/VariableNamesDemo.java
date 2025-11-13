aa9e18fcc2d03a0daca8b703f6391b3a424e9a73
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-11-13 23:23:36 +0530

    variables

---
+++ src/VariableNamesDemo.java
@@ -0,0 +1,14 @@
+public class VariableNamesDemo {
+    public static  void main(String[] args){
+        int age=25;
+        int studentCunt = 100;
+        int _score = 95;
+        int $total = 500;
+        int gearRatio = 6;
+        final  int MAX_SPEED=120;
+        System.out.println("Age: " + age);
+        System.out.println("Student Count: " + studentCunt);
+        System.out.println("Gear Ratio : "+gearRatio);
+        System.out.println("Max Speed: "+ MAX_SPEED);
+    }
+}
