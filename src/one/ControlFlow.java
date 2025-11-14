546c0f04ad3d8c9f1281e0636b770f32f60ed3ac
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-11-14 18:09:20 +0530

    control flow

--- src/one/ControlFlow.java
+++ src/one/ControlFlow.java
@@ -1,5 +1,7 @@
 package one;
 
+import com.sun.security.jgss.GSSUtil;
+
 public class ControlFlow {
     public static void main(String[] args) {
         //if-else
@@ -8,6 +10,18 @@ public class ControlFlow {
             System.out.println("Welcome to our bank!");
         }else{
             System.out.println("not applicable");
+        }
+
+        //if else ladder
+        int marks=90;
+        if (marks >90){
+            System.out.println("Grade A");
+        }else if(marks>75){
+            System.out.println("Grade B");
+        } else if (marks>60) {
+            System.out.println("Grade C");
+        }else{
+            System.out.println("F");
         }
     }
 }
