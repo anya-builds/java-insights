c4f6d6634c6ebebc895345d85338eaabf2696c7e
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-11-17 14:22:46 +0530

    contructor

---
+++ src/two/constructors/Car.java
@@ -0,0 +1,16 @@
+package two.constructors;
+
+public class Car {
+    String brand;
+    String color;
+    int speed;
+
+    public Car(){
+        System.out.println("Constructor Called");
+    }
+
+    public  void drive(){
+        System.out.println(brand + " is driving at "+ speed);
+    }
+
+}
---
+++ src/two/constructors/ConstructorsDemo.java
@@ -0,0 +1,21 @@
+package two.constructors;
+
+public class ConstructorsDemo {
+    public static void main(String[] args) {
+        Car car1=new Car();
+//        car1.speed=100;
+//        car1.brand="Toyota";
+//        car1.color="Red";
+        car1.drive();
+
+        Car car2=new Car();
+//        car2.speed=120;
+//        car2.brand="kia";
+//        car2.color="White";
+        car2.drive();
+
+//        Date date = new Date();
+//        date.
+
+    }
+}
