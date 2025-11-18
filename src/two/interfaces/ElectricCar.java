289bfc26e00c75e78e87964c7f3adb851694d79b
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-11-18 18:42:51 +0530

    interfaces

---
+++ src/two/interfaces/ElectricCar.java
@@ -0,0 +1,22 @@
+package two.interfaces;
+
+import java.sql.SQLOutput;
+
+public class ElectricCar implements CarControls {
+
+    @Override
+    public void turnRight() {
+        System.out.println("ELECTRIC RIGHT");
+    }
+
+    @Override
+    public void turnLeft() {
+        System.out.println("ELECTRIC LEFT");
+    }
+
+    @Override
+    public void applyBrakes() {
+        System.out.println("ELECTRIC BRAKES");
+
+    }
+}
