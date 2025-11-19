fac41353dc0c257e03edac207dc9b6a72074cef5
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-11-19 15:27:57 +0530

    try-catch-finally

--- src/four/LibraryDemo.java
+++ src/four/LibraryDemo.java
@@ -1,4 +1,26 @@
 package four;
 
+import java.util.List;
+
+class Library{
+    int availableBooks = 3;
+    public void borrowBook(int booksRequested){
+        try{
+            int[] books={101,102,103};
+            System.out.println("Book Requested: " + books[booksRequested]);
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("exception occurred");
+        }catch (NullPointerException e){
+            System.out.println("Null exception");
+        }finally {
+            System.out.println("finally");
+        }
+    }
+}
 public class LibraryDemo {
+    static void main(String[] args) {
+        Library library=new Library();
+        library.borrowBook(3);
+
+    }
 }
