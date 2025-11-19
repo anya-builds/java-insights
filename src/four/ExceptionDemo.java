b8a0530f0334c079dd1063f997c6445c58ac0959
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-11-19 13:35:56 +0530

    exceptions

--- src/four/ExceptionDemo.java
+++ src/four/ExceptionDemo.java
@@ -2,14 +2,23 @@ package four;
 
 public class ExceptionDemo {
     static void main(String[] args) {
-        int a=10;
-        int result = 10/0;
-        System.out.println("Done");
+//        int a=10;
+//        int result = 10/0;
+//        System.out.println("Done");
 
         try{
-            
-        }catch (Exception e){
-
+            int result=10/0;
+        }catch (ArithmeticException e){
+            System.out.println("In catch block");
         }
+        System.out.println("Done");
+        int[] a={1,2,3};
+//        System.out.println(a[6]);
+        try{
+            System.out.println(a[6]);
+        }catch(IndexOutOfBoundsException e){
+            System.out.println("exception");
+        }
+        System.out.println("final");
     }
 }
