14853ad55e13313cd6b5768a63fc656a175d044f
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-11-19 13:26:07 +0530

    exceptions

---
+++ src/four/ExceptionDemo.java
@@ -0,0 +1,15 @@
+package four;
+
+public class ExceptionDemo {
+    static void main(String[] args) {
+        int a=10;
+        int result = 10/0;
+        System.out.println("Done");
+
+        try{
+            
+        }catch (Exception e){
+
+        }
+    }
+}
