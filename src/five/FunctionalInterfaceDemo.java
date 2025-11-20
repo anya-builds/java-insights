cd23a471e3c52aa6cf21c2fced1b022cddb8fdda
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-11-20 23:06:15 +0530

    functional interface

--- src/five/FunctionalInterfaceDemo.java
+++ src/five/FunctionalInterfaceDemo.java
@@ -3,9 +3,20 @@ package five;
 @FunctionalInterface
 interface BookAction{
     void perform();
+//    void performance();
 }
+
+
+// ()->{}
 public class FunctionalInterfaceDemo {
     static void main(String[] args) {
+        BookAction action=new BookAction() {
+            @Override
+            public void perform() {
+                System.out.println("Action performed");
+            };
 
+        };
+        action.perform();
     }
 }
