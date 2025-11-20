87883bd25dd6a895f9cdc7f166c976a456fd7c58
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-11-20 23:37:57 +0530

    functional interface

--- src/five/inbuilt/FunctionInterfaceDemo.java
+++ src/five/inbuilt/FunctionInterfaceDemo.java
@@ -4,9 +4,12 @@ import java.util.function.Function;
 
 public class FunctionInterfaceDemo {
     public static Function<Integer,Integer> addFunction = (a) -> a+3;
+    public static Function<Integer,Integer> substractFunction = (a) -> a - 7;
+
 
     public static void main(String[] args){
         System.out.println(addFunction.apply(10));
+        System.out.println(substractFunction.apply(10));
 
     }
 }
