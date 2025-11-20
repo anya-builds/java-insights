9978820fb61e8f822fcb5d4872fd1d2eeaf3eab3
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-11-20 23:32:25 +0530

    functional interface

--- src/five/inbuilt/FunctionInterfaceDemo.java
+++ src/five/inbuilt/FunctionInterfaceDemo.java
@@ -1,4 +1,12 @@
 package five.inbuilt;
 
+import java.util.function.Function;
+
 public class FunctionInterfaceDemo {
+    public static Function<Integer,Integer> addFunction = (a) -> a+3;
+
+    public static void main(String[] args){
+        System.out.println(addFunction.apply(10));
+
+    }
 }
