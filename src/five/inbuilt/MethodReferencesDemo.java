e35ba8b2e968410e9bbb27f0ffe3245ee272491e
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-11-21 22:04:11 +0530

    method references

--- src/five/inbuilt/MethodReferencesDemo.java
+++ src/five/inbuilt/MethodReferencesDemo.java
@@ -4,6 +4,7 @@ import com.sun.security.jgss.GSSUtil;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class MethodReferencesDemo {
    public static void main(String[] args) {
@@ -15,5 +16,16 @@ public class MethodReferencesDemo {
 
        for(String name:names)
            System.out.println(name);
+
+//       names.forEach(new Consumer<String>() {
+//           @Override
+//           public void accept(String s) {
+//               System.out.println(s);
+//           }
+//       });
+
+       names.forEach((String name)->{
+           System.out.println(name);
+       });
     }
 }
