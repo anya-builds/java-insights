55be3c1c969b01ae51215a6978078a906d084da8
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-11-21 22:29:51 +0530

    Bi Consumer

---
+++ src/five/inbuilt/BiConsumerDemo.java
@@ -0,0 +1,9 @@
+package five.inbuilt;
+
+import java.util.function.BiConsumer;
+public class BiConsumerDemo {
+    public static void main(String[] args) {
+        BiConsumer<Integer, Integer> addConsumer = (a,b) -> System.out.println("Addition : "+  (a + b));
+        addConsumer.accept(10, 30);
+    }
+}
