28666e6e44a941ad66a8a7fb259a94d9bba3af8c
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-11-21 22:23:10 +0530

    Consumer demo

--- src/five/inbuilt/ConsumerDemo.java
+++ src/five/inbuilt/ConsumerDemo.java
@@ -5,8 +5,13 @@ import java.util.function.Consumer;
 public class ConsumerDemo {
     public static void main(String[] args) {
 //        Consumer<String> consumer1=(str)-> System.out.println(str);
+//        Consumer<String> consumer2=(str)-> System.out.println(str);
         Consumer<String> consumer1 = System.out::println;
+        Consumer<String> consumer2= str -> System.out.println("Length: " + str.length());
         consumer1.accept("Hello");
         consumer1.accept("Hi");
+
+        consumer2.accept("Hello");
+        consumer2.accept("Hi");
     }
 }
