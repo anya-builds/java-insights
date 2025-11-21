7c3ac759a81dd4c2c1fe5c4aa0d7180960e5c446
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-11-21 21:58:26 +0530

    method references

--- src/five/inbuilt/MethodReferencesDemo.java
+++ src/five/inbuilt/MethodReferencesDemo.java
@@ -1,8 +1,14 @@
 package five.inbuilt;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MethodReferencesDemo {
    public static void main(String[] args) {
         //::
-       
+        List<String> names= Arrays.asList("Alice","Charlie","Bob");
+       for (int i = 0; i < names.size(); i++) {
+           System.out.println(names.get(i));
+       }
     }
 }
